af6a01434a03962413df71d16a7ed817f6da2565
JihanN-arch <jnpermata07@gmail.com>
2026-09-22 08:14:11 +0700

    chore: update project configuration

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f1$nx*!l0^d=mhhy3&oz-tghb+5(oh!+sp#z7v9c2o9rwu&p81'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jihan-nabiilah-config.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jihan-nabiilah-nanem.pws.cs.ui.ac.id"]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
